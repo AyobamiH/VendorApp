3eec7acb03c22e0f13c24130f9d6ea625ea45d96
AyobamiH <heryourbarme@live.com>
2022-11-18 12:08:59 +0000

    editprofile route

--- routes/main.js
+++ routes/main.js
@@ -14,5 +14,6 @@ router.post("/login", authController.postLogin);
 router.get("/logout", authController.logout);
 router.get("/signup", authController.getSignup);
 router.post("/signup", authController.postSignup);
+router.get("/profile/editprofile", ensureAuth, postsController.editProfile);
 
 module.exports = router;
