2205433aabf5d0bfc58f3a153d812863a56bc84e
AyobamiH <heryourbarme@live.com>
2022-11-21 22:08:18 +0000

    comments, passport-facebook authentication strategy, passport-instagram authentication strategy

--- config/passport.js
+++ config/passport.js
@@ -1,8 +1,16 @@
 const LocalStrategy = require("passport-local").Strategy;
 const GoogleStrategy = require('passport-google-oauth20').Strategy
+const FacebookStrategy = require('passport-facebook').Strategy
+const InstagramStrategy = require('passport-instagram').Strategy
 const mongoose = require("mongoose");
 const User = require("../models/User");
 const GoogleUser = require('../models/GoogleUser')
+const FacebookUser = require('../models/FacebookUser');
+const InstagramUser = require("../models/InstagramUser");
+
+
+// Configure the passport Local strategy.
+//
 
 module.exports = function (passport) {
   passport.use(
@@ -49,11 +57,25 @@ module.exports = function (passport) {
           } else {
              GoogleUser.findById(id, function(err, user){
              if(err) done(err);
-             done(null, user);
-          })
-      }
-   })
-  })
+             if(user){
+              done(null, user);
+            } else {
+               FacebookUser.findById(id, function(err, user){
+               if(err) done(err);
+               if(user){
+                done(null, user);
+              } else {
+                 InstagramUser.findById(id, function(err, user){
+                 if(err) done(err);
+                 done(null, user);
+                })
+              }
+              })
+            } 
+            })
+          } 
+      })
+    })
 
 
   passport.use(
@@ -87,6 +109,54 @@ module.exports = function (passport) {
       }
     )
   )
+
+  passport.use(
+    new FacebookStrategy(
+      {
+        clientID: process.env.FACEBOOK_CLIENT_ID,
+        clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+        callbackURL:  "/auth/facebook/callback"
+
+       
+      },
+      async (accessToken, refreshToken, profile, done) => {
+        console.log(profile)
+        const newUser = {
+          facebookId: profile.id,
+          userName: profile.displayName.split(' ')[0] + profile.displayName.split(' ')[1],
+          displayName: profile.displayName,
+          firstName: profile.displayName.split(' ')[0],
+          lastName: profile.displayName.split(' ')[1],
+          image: profile.photos
+        }
+
+        try {
+          let user = await FacebookUser.findOne({ facebookId: profile.id })
+
+          if (user) {
+            done(null, user)
+          } else {
+            user = await FacebookUser.create(newUser)
+            done(null, user)
+          }
+        } catch (err) {
+          console.error(err)
+        }
+      }
+    )
+  )
+  passport.use(new InstagramStrategy({
+    clientID: process.env.INSTAGRAM_CLIENT_ID,
+    clientSecret: process.env.INSTAGRAM_CLIENT_SECRET,
+    callbackURL: "auth/instagram/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    User.findOrCreate({ instagramId: profile.id }, function (err, user) {
+      return done(err, user);
+    });
+  }
+));
+
 }
 
   
